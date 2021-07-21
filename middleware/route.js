f5d360238f2d328cabab5b2f4a772941533eb1fa
msoler75 <msoler75@yahoo.es>
2021-07-21 17:59:43 -0400

    setups

--- middleware/route.js
+++ middleware/route.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 export default function ({ res, store }) {
-=======
-export default function ({ route, store }) {
->>>>>>> 5b926bc2a1de7e3a306baf12ce3b4a592763b686
     // https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-server#timing
     res.timing.start('midd', 'Middleware timing description')
     
