e88a0d79700972f62e0aea8c9a035bba43a43153
msoler75 <msoler75@yahoo.es>
2021-08-16 11:37:20 -0400

    articulo con imagenes responsive

--- mixins/vercontenido.js
+++ mixins/vercontenido.js
@@ -74,12 +74,42 @@ export default {
   methods: {
   renderMarkdown(md) {
     var html = this.$md.render(md)
-    console.warn(html)
+    // console.warn(html)
     html = html
       .replace(/(<img[^>]+>)<br \/>\n?\s*(<img)/gm, '$1\n$2')
       .replace(/(<img[^>]+>)<br \/>\n?\s*(<img)/gm, '$1\n$2')
       .replace(/<p[^>]*>(?:<strong>)?((?:\s*<img[^>]+>[\s\n]*)+)(?:<\/strong>)?<\/p>/gm, '$1')
       .replace(/<p[^>]*>(<img[^>]+>)<br\s*\/?>\n(.+?)<\/p>/g, '<figure>$1<figcaption>$2</figcaption></figure>')
+      .replace(/<img[^>]+>/g, (p0, p1, offset, s) =>
+      {
+        const data = p0.match(/src=['"]([^'"]+)['"]/)
+        if(!data||!data[1]) return p0
+        const src = this.$img(data[1], {sizes: "xs:100vw xm:100vw sm:100vw md:100vw lg:100vw xl:100vw"})
+        if(!src) return p0
+        const xs = this.$img(data[1], {width: 320})
+        const xm = this.$img(data[1], {width: 480})
+        const sm = this.$img(data[1], {width: 640})
+        const md = this.$img(data[1], {width: 768})
+        const lg = this.$img(data[1], {width: 1024})
+        // const xl = this.$img(data[1], {fit:'cover', width: 1280})
+        // const xxl = this.$img(data[1], {fit:'cover', width: 1536})
+        return  `<picture loading='lazy'>
+  <source media="(min-width: 320px)"
+          sizes="100vw"
+          srcset="${xs} 320w,
+                  ${xm} 480w,
+                  ${sm} 640w,
+                  ${md} 768w,
+                  ${lg} 1024w">
+  <img src="${lg}" loading='lazy'
+       sizes="100vw"
+       srcset="${xs} 320w,
+              ${xm} 480w,
+              ${sm} 640w,
+              ${md} 768w,
+              ${lg} 1024w">
+</picture>`
+      })
     return html
   }
 }
