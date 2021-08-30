d87b8730427a6cc409a26c3258f45e49adc30f20
msoler75 <msoler75@yahoo.es>
2021-08-30 10:40:17 -0400

    agregado eventos

--- pages/boletines/_id.vue
+++ pages/boletines/_id.vue
@@ -1,48 +1,78 @@
 <template>
-<section class="max-w-md mx-auto">
-  <h1>{{boletin.titulo}}</h1>
-  <h2>Noticias</h2>
-  <Grid>
-    <Card v-for="noticia of boletin.noticias"
-        :data="noticia"
-        :key="'noticia-'+noticia.id"
-        collection="noticias"          
-      />
-  </Grid>
-  <h2>Comunicados</h2>
-  <Grid>
-    <Card v-for="comunicado of boletin.comunicados"
-        :data="comunicado"
-        :key="'comunicado-'+comunicado.id"
-        collection="comunicados"          
-      />
-  </Grid>
-  <h2>Blogs</h2>
-  <Grid>
-    <Card v-for="entrada of boletin.entradas"
-        :data="entrada"
-        :key="'entrada-'+entrada.id"
-        collection="entradas"          
-      />
-  </Grid>
-  <h2>Libros</h2>
-  <Grid>
-    <Card v-for="libro of boletin.libros"
-        :data="libro"
-        :key="'libro-'+libro.id"
-        collection="libros"          
-      />
-  </Grid>
-  <h2>Audios</h2>
-  <Grid>
-    <Card v-for="audio of boletin.audios"
-        :data="audio"
-        :key="'audio-'+audio.id"
-        collection="audios"          
-      />
-  </Grid>
+  <section class="max-w-md mx-auto">
+    <h1>{{ boletin.titulo }}</h1>
+    <template v-if="boletin.noticias.length">
+      <h2>Noticias</h2>
+      <Grid>
+        <Card
+          v-for="noticia of boletin.noticias"
+          :data="noticia"
+          :key="'noticia-' + noticia.id"
+          collection="noticias"
+        />
+      </Grid>
+    </template>
 
-</section>
+    <template v-if="boletin.eventos.length">
+      <h2>Eventos</h2>
+      <Grid>
+        <Card
+          v-for="evento of boletin.eventos"
+          :data="evento"
+          :key="'evento-' + evento.id"
+          collection="eventos"
+        />
+      </Grid>
+    </template>
+
+    <template v-if="boletin.comunicados.length">
+      <h2>Comunicados</h2>
+      <Grid>
+        <Card
+          v-for="comunicado of boletin.comunicados"
+          :data="comunicado"
+          :key="'comunicado-' + comunicado.id"
+          collection="comunicados"
+        />
+      </Grid>
+    </template>
+
+    <template v-if="boletin.entradas.length">
+      <h2>Blogs</h2>
+      <Grid>
+        <Card
+          v-for="entrada of boletin.entradas"
+          :data="entrada"
+          :key="'entrada-' + entrada.id"
+          collection="entradas"
+        />
+      </Grid>
+    </template>
+
+    <template v-if="boletin.libros.length">
+      <h2>Libros</h2>
+      <Grid>
+        <Card
+          v-for="libro of boletin.libros"
+          :data="libro"
+          :key="'libro-' + libro.id"
+          collection="libros"
+        />
+      </Grid>
+    </template>
+
+    <template v-if="boletin.audios.length">
+      <h2>Audios</h2>
+      <Grid>
+        <Card
+          v-for="audio of boletin.audios"
+          :data="audio"
+          :key="'audio-' + audio.id"
+          collection="audios"
+        />
+      </Grid>
+    </template>
+  </section>
 </template>
 
 <script>
@@ -51,18 +81,18 @@ export default {
   mixins: [vercontenidomixin],
   async asyncData({ app, $strapi, route, redirect }) {
     try {
-      const id = route.params.id
-      const boletines = await $strapi.find('boletines', id.match(/\d+/)?{id}:{slug:id})
-      const contenido = boletines[0]
+      const id = route.params.id;
+      const boletines = await $strapi.find(
+        "boletines",
+        id.match(/\d+/) ? { id } : { slug: id }
+      );
+      const contenido = boletines[0];
       return { contenido, boletin: contenido };
+    } catch (error) {
+      console.error(error);
     }
-    catch(error)
-    {
-      console.error(error)
-    }
-  },
+  }
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
