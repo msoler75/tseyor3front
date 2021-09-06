e1d1af35e2fbbe96e82e8c826d9c7553b7c50f8c
msoler75 <msoler75@yahoo.es>
2021-09-06 15:06:18 -0300

    fix

--- pages/agenda/index.vue
+++ pages/agenda/index.vue
@@ -4,22 +4,24 @@
     <div class="lg:flex lg:justify-center lg:items-start mx-auto">
       <Card class="order-2 p-5 lg:ml-12 mb-12">
         <h3 class="text-center hidden sm:block">Horarios Regulares</h3>
-        <div class="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-1 gap-3">
-        <div v-for="(a, index) of agenda" :key="index" class="my-3">
-          <Horario :data="a.horario" />
-          <NLink
-            class="font-bold whitespace-nowrap text-blue-600"
-            :to="'/actividades/' + a.actividad.id"
-            >{{ a.actividad.titulo }}</NLink
-          >
-          de
-          <NLink
-            class="font-bold whitespace-nowrap px-3 py-1 rounded-lg shadow"
-            :class="equipos.find(x => x.id === a.equipo.id).color"
-            :to="'/equipos/' + a.equipo.id"
-            >{{ a.equipo.nombre }}</NLink
-          >
-        </div>
+        <div
+          class="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-1 gap-3"
+        >
+          <div v-for="(a, index) of agenda" :key="index" class="my-3">
+            <Horario :data="a.horario" />
+            <NLink
+              class="font-bold whitespace-nowrap text-blue-600"
+              :to="'/actividades/' + a.actividad.id"
+              >{{ a.actividad.titulo }}</NLink
+            >
+            de
+            <NLink
+              class="font-bold whitespace-nowrap px-3 py-1 rounded-lg shadow"
+              :class="equipos.find(x => x.id === a.equipo.id).color"
+              :to="'/equipos/' + a.equipo.id"
+              >{{ a.equipo.nombre }}</NLink
+            >
+          </div>
         </div>
       </Card>
 
@@ -44,35 +46,46 @@
         </div>
 
         <div class="grid proximas gap-2">
-        <template
-          v-for="(a, index) of proximasFiltro"
-        >
-        <Card v-if="a.seccion" :key="'a'+index" class="col-span-4 p-5 font-bold text-3xl text-center tracking-widest uppercase">
-            {{a.seccion}}
-        </Card>
-            
-        <Card :key="'b'+index" class="p-3 text-3xl flex font-bold justify-center items-center">{{ a.fecha.dia }}</Card> 
+          <template v-for="(a, index) of proximasFiltro">
+            <Card
+              v-if="a.seccion"
+              :key="'a' + index"
+              class="col-span-4 p-5 font-bold text-3xl text-center tracking-widest uppercase"
+            >
+              {{ a.seccion }}
+            </Card>
 
-        <Card :key="'c'+index" class="p-3 text-center">
-            <span>{{ a.fecha.diasemana }}</span>
-            <span class="font-bold text-xl">{{ $dayjs("1970/1/1 " + a.hora).format("HH:mm") }}</span>
-        </Card>  
+            <Card
+              :key="'b' + index"
+              class="p-3 text-3xl flex font-bold justify-center items-center"
+              >{{ a.fecha.dia }}</Card
+            >
 
-        <NLink :key="'d'+index"
-            class="card rounded font-bold text-center text-blue-600 flex justify-center items-center"
-            :to="'/actividades/' + a.detalles.actividad.id">
-            {{ a.detalles.actividad.titulo }}
-        </NLink>
-        
-        <NLink :key="'e'+index"
-            class="font-bold text-center px-3 py-1 rounded shadow flex justify-center items-center"
-            :class="equipos.find(x => x.id === a.detalles.equipo.id).color"
-            :to="'/equipos/' + a.detalles.equipo.id">
-            {{ a.detalles.equipo.nombre }}
-        </NLink>
-        
-        </template>
-        <div class="col-span-4 my-5 text-3xl text-center">...</div>
+            <Card :key="'c' + index" class="p-3 text-center">
+              <span>{{ a.fecha.diasemana }}</span>
+              <span class="font-bold text-xl">{{
+                $dayjs("1970/1/1 " + a.hora).format("HH:mm")
+              }}</span>
+            </Card>
+
+            <NLink
+              :key="'d' + index"
+              class="card rounded font-bold text-center text-blue-600 flex justify-center items-center"
+              :to="'/actividades/' + a.detalles.actividad.id"
+            >
+              {{ a.detalles.actividad.titulo }}
+            </NLink>
+
+            <NLink
+              :key="'e' + index"
+              class="font-bold text-center px-3 py-1 rounded shadow flex justify-center items-center"
+              :class="equipos.find(x => x.id === a.detalles.equipo.id).color"
+              :to="'/equipos/' + a.detalles.equipo.id"
+            >
+              {{ a.detalles.equipo.nombre }}
+            </NLink>
+          </template>
+          <div class="col-span-4 my-5 text-3xl text-center">...</div>
         </div>
       </div>
     </div>
@@ -110,19 +123,21 @@ export default {
     };
   },
   computed: {
-      proximasFiltro() {
-          let seccion = null
-          return this.proximas.filter(a=>this.equipos.find(x => x.id === a.detalles.equipo.id).viendo)
-          // ponemos la propiedad de seccion (mes actual) en los elementos
-          .map(a=>{
-          if(a.fecha.mesnombre!==seccion)
-          {
-              a.seccion = a.fecha.mesnombre
-              seccion = a.fecha.mesnombre
+    proximasFiltro() {
+      let seccion = null;
+      return this.proximas
+        .filter(
+          a => this.equipos.find(x => x.id === a.detalles.equipo.id).viendo
+        )
+        .map(a => {
+          a.seccion = null;
+          if (a.fecha.mesnombre !== seccion) {
+            a.seccion = a.fecha.mesnombre;
+            seccion = a.fecha.mesnombre;
           }
-          return a
-          })
-      }
+          return a;
+        }); // ponemos la propiedad de seccion (mes actual) en los elementos
+    }
   },
   mounted() {
     const dia_semana = [
@@ -132,7 +147,7 @@ export default {
       "miercoles",
       "jueves",
       "viernes",
-      "sabado",
+      "sabado"
     ];
     const meses = [
       "Enero",
@@ -160,7 +175,16 @@ export default {
       const diadelmes = fecha.date();
       const semana = Math.floor(diadelmes / 7);
       const ssemana = ordinal[semana];
-      console.log('i', i, 'semana', semana, ssemana, sdia, 'diadelmes', diadelmes)
+      console.log(
+        "i",
+        i,
+        "semana",
+        semana,
+        ssemana,
+        sdia,
+        "diadelmes",
+        diadelmes
+      );
       for (const item of this.agenda) {
         let ok = false;
         let semi = false;
@@ -168,17 +192,23 @@ export default {
           // todos los lunes
           ok = true;
         } else if (item.horario.dia.startsWith(sdia)) {
-            console.log(item.horario.dia, 'startsWith', sdia)
+          console.log(item.horario.dia, "startsWith", sdia);
           if (item.horario.dia.search("sin_definir") > -1) {
             // ok = true
             semi = true;
           } else if (item.horario.dia.search(ssemana) > -1) {
-              console.log('horario', item.horario, 'ssemana', ssemana, 'sdia', sdia)
+            console.log(
+              "horario",
+              item.horario,
+              "ssemana",
+              ssemana,
+              "sdia",
+              sdia
+            );
             ok = true;
           }
         }
-        if (ok)
-        {
+        if (ok) {
           this.proximas.push({
             fecha: {
               diasemana: sdia.replace("erc", "érc").replace("aba", "ába"),
@@ -200,6 +230,6 @@ export default {
 
 <style scoped>
 .proximas {
-    grid-template-columns: 60px 2fr 2fr 2fr;
+  grid-template-columns: 60px 2fr 2fr 2fr;
 }
 </style>
