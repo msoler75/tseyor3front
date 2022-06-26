fd8ef98db8f1a7994db94ad3877008cd4c532631
msoler75 <msoler75@yahoo.es>
2022-06-26 11:45:03 +0200

    strapi cleaning code

--- plugins/strapiplugin.js
+++ plugins/strapiplugin.js
@@ -1,86 +1,88 @@
 const strapiv4 = require('./strapi.js');
 
+class strapiv4extended extends strapiv4 {
+
+  constructor(strapiUrl, store) {
+    super(strapiUrl, store)
+  }
+
+  filterByIdSlug(id, params) {
+    const {
+      filters,
+      populate
+    } = params || {}
+    return {
+      ...params,
+      filters: id.match(/^\d+$/) ? {
+        id: {
+          ...filters,
+          $eq: id
+        }
+      } : {
+        ...filters,
+        slug: {
+          $eq: id
+        }
+      },
+      populate: populate == '*' ? '*' : {
+        ...populate,
+        imagen: {
+          fields: ['url', 'width', 'height']
+        }
+      }
+    }
+  }
+
+  filterByList(params) {
+    const {
+      sort,
+      populate
+    } = params || {}
+    return {
+      ...params,
+      populate: populate == '*' ? '*' : {
+        ...populate,
+        imagen: {
+          fields: ['url', 'width', 'height']
+        }
+      },
+      sort: sort ? sort : ['publishedAt:desc']
+    }
+  }
+
+
+  getCollectionFromRoute(route) {
+    console.log('GETCOLLECTION FROM', route.path)
+    const parts = route.path.split('/')
+    console.log('parts', parts)
+    return parts[1].replace("usuarios", "users")
+  }
+
+  async getContent(route, params) {
+    const collection = this.getCollectionFromRoute(route)
+    const response = await this.find(collection, this.filterByIdSlug(route.params.id, params))
+    console.log('GETCONTENT RESPONSE', response)
+    let data = response.data ? response.data : response
+    return Array.isArray(data) ? data[0] : data
+  }
+
+  async findList(route, params) {
+    const collection = this.getCollectionFromRoute(route)
+    return this.find(collection, this.filterByList(params))
+  }
+
+
+}
+
+
+
+
 
 export default ({
   $strapi,
   $config,
   store,
 }, inject) => {
-
-
-  class strapiv4extended extends strapiv4{
-
-    constructor(strapiUrl, store) {
-        super(strapiUrl, store)
-    }
-
-    filterByIdSlug(id, params) {
-      const {
-        filters,
-        populate
-      } = params || {}
-      return {
-        ...params,
-        filters: id.match(/^\d+$/) ? {
-          id: {
-            ...filters,
-            $eq: id
-          }
-        } : {
-          ...filters,
-          slug: {
-            $eq: id
-          }
-        },
-        populate: populate == '*' ? '*' : {
-          ...populate,
-          imagen: {
-            fields: ['url', 'width', 'height']
-          }
-        }
-      }
-    }
-
-    filterByList(params) {
-      const {
-        sort,
-        populate
-      } = params || {}
-      return {
-        ...params,
-        populate: populate == '*' ? '*' : {
-          ...populate,
-          imagen: {
-            fields: ['url', 'width', 'height']
-          }
-        },
-        sort: sort ? sort : ['publishedAt:desc']
-      }
-    }
-
-
-    getCollectionFromRoute(route) {
-      console.log('GETCOLLECTION FROM', route.path)
-      const parts = route.path.split('/')
-      console.log('parts', parts)
-      return parts[1].replace("usuarios", "users")
-    }
-
-    async getContent(route, params) {
-      const collection = this.getCollectionFromRoute(route)
-      const response = await this.find(collection, this.filterByIdSlug(route.params.id, params))
-      console.log('GETCONTENT RESPONSE', response)
-      let data = response.data ? response.data : response
-      return Array.isArray(data) ? data[0] : data
-    }
-
-    async findList(route, params) {
-      const collection = this.getCollectionFromRoute(route)
-      return this.find(collection, this.filterByList(params))
-    }
-
-
-  }
 
   const strapiv4ext = new strapiv4extended($config.strapiUrl, store)
 
